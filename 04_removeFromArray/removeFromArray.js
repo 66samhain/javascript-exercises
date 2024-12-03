acb3653385e0e49d8f4267db8236ab237e3ec3c7
66samhain <aktr9886@protonmail.com>
2024-12-03 09:08:39 +0200

    removeFromArray solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+  let newArray = []
+
+  for (let j = 0; j < array.length; j++) {
+    if (!elementsToRemove.includes(array[j])) {
+      newArray.push(array[j])
+    }
+  }
+
+  return newArray
 };
 
 // Do not edit below this line
