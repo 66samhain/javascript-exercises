79d426c009b9df3d5a8fb4561cae4c63b9616fbb
66samhain <aktr9886@protonmail.com>
2024-12-03 08:30:26 +0200

    repeatString solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, timesToRepeat) {
+  if (timesToRepeat < 0) {
+    return 'ERROR'
+  }
 
+  let repeatedString = []
+
+  for (let i = 0; i < timesToRepeat; i++) {
+    repeatedString.push(stringToRepeat)
+  }
+
+  return repeatedString.join('')
 };
 
 // Do not edit below this line
