c85ff0224d73f1782863690a6cb330f4d19f638a
66samhain <aktr9886@protonmail.com>
2025-01-26 15:45:24 +0200

    calculator solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum = sum + numbers[i]
+  }
+
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  let product = 1;
 
+  for (let i = 0; i < numbers.length; i++) {
+    product = product * numbers[i]
+  }
+
+  return product
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1
+
+  for (let i = num; i >= 1; i--) {
+    factorial = factorial * i
+  }
+
+  return factorial
 };
 
 // Do not edit below this line
