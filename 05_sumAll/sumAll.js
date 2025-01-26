04db1b72980cb67409c538191a2eac508ed206f2
66samhain <aktr9886@protonmail.com>
2025-01-26 15:21:44 +0200

    sumAll solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0) {
+    return 'ERROR';
+  }
 
+  if (Math.floor(num1) !== num1 || Math.floor(num2) !== num2) {
+    return 'ERROR';
+  }
+
+  if (!Number(num1) || !Number(num2)) {
+    return 'ERROR';
+  }
+
+  let sum = 0;
+
+  if (num1 < num2) {
+    for (let i = num1; i <= num2; i++) {
+      sum = sum + i;
+    }
+  } else {
+    for (let i = num2; i <= num1; i++) {
+      sum = sum + i;
+    }
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
